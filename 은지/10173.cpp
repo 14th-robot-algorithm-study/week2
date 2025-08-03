7f033c830f2f523a68119b618ad3789d4488a9a3
Eunbyul <eunbyul0805@gmail.com>
2025-08-04 00:34:14 +0900

    2025.08.03
    [C++ 배우기: 브론즈2] 니모를 찾아서
    https://www.acmicpc.net/problem/10173

--- 은지/10173.cpp
+++ 은지/10173.cpp
@@ -6,10 +6,9 @@ int main() {
 	std::string str;
 
 	while (std::getline(std::cin, str) && str != "EOI") {
-		std::string line = str;
-		std::transform(line.begin(), line.end(), line.begin(), ::toupper);
+		std::transform(str.begin(), str.end(), str.begin(), ::toupper);
 
-		if (line.find("NEMO") != std::string::npos) {
+		if (str.find("NEMO") != std::string::npos) {
 			std::cout << "Found \n";
 		}
 		else {
